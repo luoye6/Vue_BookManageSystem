2c4011dc6bff7dca846021364aecd3b8d63edafc
落叶无情 <909088445@qq.com>
2023-02-24 13:20:43 +0000

    update src/main-prod.js.
    Signed-off-by: 落叶无情 <909088445@qq.com>

--- src/main-prod.js
+++ src/main-prod.js
@@ -36,7 +36,7 @@ Vue.use(vueBaberrage)
 //导入axios
 import axios from 'axios'
 //配置请求的根路径
-axios.defaults.baseURL = 'http://cba.xiaobaitiao.club:443/api/'
+axios.defaults.baseURL = 'xxxxxxxx服务器地址xxxxx'
 Vue.prototype.$http = axios
 // const CancelToken = axios.CancelToken;
 // const source = CancelToken.source();
